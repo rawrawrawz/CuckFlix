1f53f0d089b2d620525bf5bbd83e3e7420da4310
Gabriel <50469852+rawrawrawz@users.noreply.github.com>
2020-07-28 21:10:43 -0300

    Primeiro Upload

---
+++ src/components/Menu/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/logo.png'
+import './Menu.css'
+import Button from '../Button';
+//import ButtonLink from './components/ButtonLink';
+
+
+function Menu(){
+    return (
+      <nav className="Menu">
+          <Link to="/">          
+            <img className="Logo" src={Logo} alt="CuckFlix logo"/>
+          </Link>
+
+          <Button as={Link} className="ButtonLink" to="/cadastro/video">
+              Novo Video
+          </Button>
+      </nav>  
+    );
+}
+
+export default Menu;
